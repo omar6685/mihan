e840f827447c32379cae8addd1e90d96d0890a20
omar6685 <omr6685@hotmail.com>
2024-02-11 17:43:05 +0300

    add gudie scaffold 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_08_151344) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_11_143335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "guides", force: :cascade do |t|
+    t.string "file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "mihan_mowatans", force: :cascade do |t|
     t.string "result"
