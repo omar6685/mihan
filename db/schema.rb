cb3efe6d8b5a3cd4330a45eb2ee884b4dd14591b
omar6685 <omr6685@hotmail.com>
2024-02-18 19:56:23 +0300

    add tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_18_162950) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_18_165107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_162950) do
     t.string "company_name"
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "status"
+    t.text "chat_history"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tickets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +63,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_162950) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tickets", "users"
 end
